af4085c4c2cf05579adc672927dd327f124f654b
Justin Chew <jchew1@byu.edu>
2024-03-18 23:25:53 -0600

    Add project files.

---
+++ Mission11_Chew/Models/EFBookstoreRespository.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Mission11_Chew.Models
+{
+    public class EFBookstoreRespository : IBookstoreRepository
+    {
+        private BookstoreContext _context;
+        public EFBookstoreRespository(BookstoreContext temp) {
+            _context = temp;
+        }
+
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
---
+++ Mission11_Chew/Models/ViewModels/BookListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mission11_Chew.Models.ViewModels
+{
+    public class BookListViewModel
+    {
+        public IQueryable<Book> Books { get; set; }
+
+        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
+    }
+}
